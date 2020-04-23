147ca8d8d859a3ecb8c34224d7be9e10d6b9c5a3
MicroOptimization <jackiezhen538@gmail.com>
2020-04-23 16:34:45 -0400

    First Commit

---
+++ discord_bot_template/src/generic_discord_bot.py
@@ -0,0 +1,37 @@
+"""
+===========================================
+Author: Codiacs
+Github: github.com/MicroOptimization
+===========================================
+"""
+import discord
+import generic_key_retriever
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+        
+        self.text_channel_names = []
+        self.text_channels = []
+        
+        for i in client.get_all_channels():
+            self.text_channel_names.append(str(i))
+            self.text_channels.append(i)
+            
+        await client.change_presence(activity=discord.Game(name="with <word>"))
+    
+    async def on_message(self, message):    
+        print('Message from {0.author} from {0.channel}: {0.content}'.format(message))
+        text = message.content
+        message_words = text.split(" ")
+        
+        await message.channel.send("'{}'".format("Hello there, I'm a bot"))
+        
+        
+
+client = MyClient()
+
+key = generic_key_retriever.get_key() #Insert your key here
+
+client.run(key)
---
+++ discord_bot_template/src/generic_key_retriever.py
@@ -0,0 +1,10 @@
+"""
+===========================================
+Author: Codiacs
+Github: github.com/MicroOptimization
+===========================================
+"""
+
+def get_key():
+    key = "<Insert key here>"
+    return key
